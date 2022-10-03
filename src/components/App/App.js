c9853effec00f5a05d29bd47de565bd6b8776de1
Valerie K <valerik210695@gmail.com>
2022-10-03 13:00:39 +0300

    create react app

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page">
+        <header className="header"></header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
